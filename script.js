b8c4c81f2bab29fdb0989df5db650f9053ab28c7
Florian Jünemann <compufloh@web.de>
2022-01-26 23:15:21 +0100

    Button-Enable

--- script.js
+++ script.js
@@ -65,12 +65,15 @@ function answer(selection) {
     console.log('selectedQuestionNumber is', selectedQuestionNumber);
     console.log('Current question is', question['right_answer']);
 
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
     if(selectedQuestionNumber == question['right_answer']){
         console.log('Richtige Antwort!!');
         document.getElementById(selection).parentNode.classList.add('bg-success');
     }else{
         console.log('Falsche Antwort!!');
         document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
-
+    document.getElementById('next-button').disabled = false;
 }
