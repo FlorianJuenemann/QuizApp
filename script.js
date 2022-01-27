a24f118a88428391c33c578edd026046446d0d78
Florian Jünemann <compufloh@web.de>
2022-01-27 11:38:47 +0100

    Button-Reset

--- script.js
+++ script.js
@@ -77,3 +77,22 @@ function answer(selection) {
     }
     document.getElementById('next-button').disabled = false;
 }
+
+function nextQuestion(){
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetButtons();
+    showQuestion();
+}
+
+function resetButtons(){
+
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
